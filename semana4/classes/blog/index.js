5e940702e2f1483a173fd85e5c8d60e41d3c09bc
Karol <karolroberta92@hotmail.com>
2020-02-05 18:03:00 -0300

    Passo 1

---
+++ semana4/classes/blog/index.js
@@ -0,0 +1,47 @@
+class Post{
+    constructor(nome, autor, biografia){
+        this.nome = nome 
+        this.autor = autor
+        this.biografia= biografia
+
+        this.escrever = () =>{
+            console.log(" Oi")
+        }
+
+    /*this.fazerAniversario = (idade) =>{
+        return idade*7*/
+    }
+
+
+
+
+}
+
+const novoPost = new post()
+post.nome =""
+post.autor =""
+post.biografia =""
+
+/*let novoCachorro = new Cachorro("Mel, 7,"viraLata")
+novoCachorro.latir()*/
+
+
+function blog(nome, autor, biografia){
+    let nome = getElementById("nome").value
+    let autor = getElementById("autor").value
+    let biografia =getElementById("biografia").value    
+    const input = document.querySelector("input").value
+    const novoBlog = input.value
+
+
+    /*if(novaTarefa ="segunda"){
+        const primeiroDia = document.getElementById("segunda")
+        listaDeMetas.innerHTML += "<li>" + novaTarefa + "</li>"
+    }else if( novaTarefa ="terca"){
+        listaDeMetas.innerHTML += "<li>" + novaTarefa +"</li>"
+        /*const listaDeCompras = document.getElementById("segunda")*/
+    
+
+
+    input.value= " "
+}
--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -8,42 +8,7 @@ function criarTarefa(){
     const listaDeMetas = document.getSelectorName("ul")*/
     
     const week = document.getElementById("semana").value
-   
-    
-    input.value =""
 
-
-<<<<<<< HEAD
-    if(semana ="segunda"){
-        const primeiroDia = document.getElementById("segunda")
-        listaDeMetas.innerHTML += "<li>" + novaTarefa + "</li>"
-    
-    }else if( semana ="terca"){
-        listaDeMetas.innerHTML += "<li>" + novaTarefa +"</li>"
-        /*const listaDeCompras = document.getElementById("segunda")*/
-    }
-    if(semana =" quarta"){
-        listaDeMetas.innerHTML += "<li>" + novaTarefa+ "</li>"
-    
-    }else if( semana= "quinta"){
-        listaDeMetas.innerHTML +="<li>" + novaTarefa+"</li>"
-    }
-    if(semana ="sexta"){
-        listaDeMetas.innerHTML += "<li>" + novaTarefa + "</li>"
-    
-    }else if( semana ="sabado"){
-    
-        listaDeMetas.innerHTML += "<li>" + novaTarefa + "</li>"
-    }
-    else{
-        semana ="domingo"
-        
-        listaDeMetas.innerHTML += "<li>" + novaTarefa + "</li>"
-    
-    
-    }
-    input.value = " "
-=======
     if(novaTarefa ="segunda"){
         const primeiroDia = document.getElementById("segunda")
         listaDeMetas.innerHTML += "<li>" + novaTarefa + "</li>"
@@ -76,5 +41,5 @@ function criarTarefa(){
 
 
     input.value =""
->>>>>>> c37d156c857b8893597a466d08fc84331fb26398
+
 }
