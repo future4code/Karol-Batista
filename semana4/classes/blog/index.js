ed2629e0cb1b9bee0d7fe5d3bbc862f74e0f816e
Karol <karolroberta92@hotmail.com>
2020-02-05 18:42:31 -0300

    Passo 3

--- semana4/classes/blog/index.js
+++ semana4/classes/blog/index.js
@@ -11,30 +11,21 @@ post.nome =""
 post.autor =""
 post.biografia =""*/
 
-/*let novoCachorro = new Cachorro("Mel, 7,"viraLata")
-novoCachorro.latir()*/
 
+
+let array = [] // monta a array 
 
 function criarBlog(){
     let novoNome = document.getElementById("nome").value
     let novoAutor = document.getElementById("autor").value
     let novaBiografia =document.getElementById("biografia").value    
     
-    const postagem = new Post(novoNome, novoAutor, novaBiografia)
-    console.log(postagem)
-    
-    
-    
-    
-    
-    /*if(novaTarefa ="segunda"){
-        const primeiroDia = document.getElementById("segunda")
-        listaDeMetas.innerHTML += "<li>" + novaTarefa + "</li>"
-    }else if( novaTarefa ="terca"){
-        listaDeMetas.innerHTML += "<li>" + novaTarefa +"</li>"
-        /*const listaDeCompras = document.getElementById("segunda")*/
-    
-
-
-    // input.value= " "
+    const postagem = new Post(novoNome, novoAutor, novaBiografia) //instancia
+    array.push(postagem)//add post no array
+    console.log(array) // imprime o array
+ 
+    //limpar os campos do formulario
+    document.getElementById("nome").value = ""
+    document.getElementById("autor").value = ""
+    document.getElementById("biografia").value = ""
 }
